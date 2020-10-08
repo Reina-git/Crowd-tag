260ee8cce28a5916de1d860b370e9a68981a7540
Reina Uhler <reinadu@gmail.comgit>
2020-10-08 12:21:33 -0700

    add-all-pages-basic

---
+++ script.js
@@ -0,0 +1,8 @@
+$("#show-delete").click(function () {
+   $("#delete-button").toggleClass("d-flex d-none");
+});
+
+$("#sign-up-button").click(function () {
+   $("#sign-up-card").toggle();
+   $("#sign-up-button").toggleClass("d-flex d-none");
+});
