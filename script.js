03bb1dde9b15fd396e49cdb81e6ad6b60ee35127
Reina Uhler <reinadu@gmail.comgit>
2020-10-12 10:01:02 -0700

    basic-template-and-char-counter

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ $("#sign-up-button").click(function () {
    $("#sign-up-card").toggle();
    $("#sign-up-button").toggleClass("d-flex d-none");
 });
+
+$("#add-photo-btn").click(function () {
+   // $("#sign-up-card").toggle();
+   $("#photo-url").toggleClass("d-none");
+});
+
+let tagInputChar = 0;
+
+$("#tag-input").keypress(function () {
+   console.log("let's add 1");
+   tagInputChar += 1;
+
+   console.log("Total inputted chars: ", tagInputChar);
+   $("#tag-char-count").html(tagInputChar);
+});
